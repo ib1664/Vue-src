8f5760c35a994ea495cb562ad297e2510cfc291b
ib1664 <ignatius1664@outlook.com>
2020-10-22 13:30:58 +1100

    proj setup

---
+++ ELEC9603-Project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="lock-image" src="./assets/index.png">
+    <login> </login>
+  </div>
+</template>
+
+<script>
+import Login from "./components/Login";
+export default {
+  components:{
+    'login': Login
+  },
+	data(){
+		return{
+		
+		}	
+	}
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ELEC9603-Project/src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3> {{ title }} </h3>
+    <input type="text" value="Email*" ref="input1" />
+	<input type="text" value="Password*" ref="input2" />
+
+	<button> Login	</button>
+  </div>
+</template>
+
+<script>
+
+export default{
+	data(){
+		return{
+			title: 'LOGIN'
+		}	
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
